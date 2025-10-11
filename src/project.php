de9d4cea835e63efbcce10df9448723e98640596
rine <sirine.elalami.22578@stu.he-ferrer.eu>
2025-10-11 16:35:36 +0200

    install plugin tailwind compo pour projet

---
+++ src/project.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section class="bg-myyellow dark:bg-mylightblue flex items-end md:mb-64 mb-32">
+    <div>
+
+    </div>
+    <div></div>
+
+</section>
+
+<?php get_footer(); ?>
