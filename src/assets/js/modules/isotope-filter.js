8e71ea8f7f0291516fde09d3b1bc84518b61ecef
rine <sirine.elalami.22578@stu.he-ferrer.eu>
2025-10-22 11:02:34 +0200

    projects new

--- src/assets/js/modules/isotope-filter.js
+++ src/assets/js/modules/isotope-filter.js
@@ -18,17 +18,17 @@ export function initIsotopeFilter() {
     });
 
     // Set the "All" button as active when page loads
-    $('.filter-btns a[data-filter="*"]').attr('aria-current', 'true');
+    $('.filter-btns a[data-filter="*"]').addClass('is-active').attr('aria-current', 'true');
 
     // Handle filter button clicks
     $('.filter-btns').on('click', 'a', function(e) {
         e.preventDefault();
 
         // Remove active state from all filter buttons
-        $('.filter-btns a').attr('aria-current', 'false');
+        $('.filter-btns a').removeClass('is-active').attr('aria-current', 'false');
 
         // Add active state to the clicked button
-        $(this).attr('aria-current', 'true');
+        $(this).addClass('is-active').attr('aria-current', 'true');
 
         // Get the filter value from data-filter attribute
         const filterValue = $(this).attr('data-filter');
--- src/projects.php
+++ src/projects.php
@@ -3,28 +3,20 @@
 Template Name: Portfolio
 */
 get_header(); ?>
-<section class="md:h-[500px] h-[25vh] bg-mybeige dark:bg-mydarkgreen flex items-end rounded-3xl">
-    <div class="pl-10 md:pl-20 pb-10 md:pb-20">
-        <h1 class="text-mydarkgreen dark:text-mybeige titre"><?php esc_html_e('Rine\'s portfolio', 'rine2'); ?></h1>
-        <h2 class="font-bold condensed texte text-mydarkgreen dark:text-mybeige max-w-56">
-            <?php esc_html_e('You\'re about to explore some of my academic and personal projects.', 'rine2'); ?>
-        </h2>
-    </div>
-    <img class="absolute md:right-1/4 md:top-[30rem] top-80 right-24 h-16 md:h-auto dark:hidden"
-        src="<?php echo get_template_directory_uri(); ?>/assets/icons/projets.svg" alt="gribouilli4">
-    <img class="absolute md:right-1/4 md:top-[30rem] top-80 right-24 h-16 md:h-auto hidden dark:block"
-        src="<?php echo get_template_directory_uri(); ?>/assets/icons/darkprojects.svg" alt="gribouilli4">
+<section class="">
 </section>
-<section class="md:pl-12 pl-6 my-10 bg-mybeige dark:bg-mydarkgreen rounded-3xl py-20">
-    <h1 class="titre text-mydarkblue dark:text-mybeige"><?php esc_html_e('Categories', 'rine2'); ?></h1>
-    <div class="flex md:flex-row flex-col gap-5 filter-btns flex-wrap">
+<section class="flex flex-col my-10 gap-5">
+    <h1 class="titre dark:text-mybeige text-myblack">
+        <span class="highlight">// </span><?php esc_html_e('MY PROJECTS', 'rine2'); ?>
+    </h1>
+    <div class="filter-btns flex px-8 gap-9">
         <a
             role="button"
             tabindex="0"
             data-filter="*"
-            class="bg-mydarkgreen dark:bg-mybeige flex flex-col justify-between items-end bg-mylightblue w-28 md:w-52 md:h-34 h-24 px-4 transition-all duration-300 ease-in-out overflow-hidden cursor-pointer">
-            <h2 class="soustitre text-mybeige dark:text-mydarkgreen text-right"><?php esc_html_e('All', 'rine2'); ?></h2>
-            <p class="texte text-mybeige dark:text-mydarkgreen text-right hidden"><?php esc_html_e('Show all projects', 'rine2'); ?></p>
+            class="w-3xs flex flex-col p-5 gap-5 rounded-2xl text-mybeige bg-myblack dark:bg-mybeige dark:text-myblack transition-all duration-300 ease-in-out overflow-hidden cursor-pointer opacity-50 [&.is-active]:opacity-100">
+            <h2 class="texte"><?php esc_html_e('All', 'rine2'); ?></h2>
+            <p class="legend hidden [.is-active_&]:block"><?php esc_html_e('Show all projects', 'rine2'); ?></p>
         </a>
         <?php
         $categories = get_terms([
@@ -37,9 +29,9 @@ get_header(); ?>
                     role="button"
                     tabindex="0"
                     data-filter=".<?php echo esc_attr($category->slug); ?>"
-                    class="bg-mydarkgreen dark:bg-mybeige flex flex-col justify-between items-end bg-mylightblue w-28 md:w-52 md:h-34 h-24 px-4 transition-all duration-300 ease-in-out overflow-hidden cursor-pointer">
-                    <h2 class="soustitre text-mybeige dark:text-mydarkgreen text-right"><?php echo esc_html($category->name); ?></h2>
-                    <p class="texte text-mybeige dark:text-mydarkgreen text-right hidden"><?php echo esc_html($category->description); ?></p>
+                    class="w-3xs flex flex-col p-5 gap-5 rounded-2xl text-mybeige bg-myblack dark:bg-mybeige dark:text-myblack transition-all duration-300 ease-in-out overflow-hidden cursor-pointer opacity-50 [&.is-active]:opacity-100">
+                    <h2 class="texte"><?php echo esc_html($category->name); ?></h2>
+                    <p class="hidden legend [.is-active_&]:block"><?php echo esc_html($category->description); ?></p>
                 </a>
             <?php endforeach;
         } ?>
@@ -47,7 +39,7 @@ get_header(); ?>
     </div>
 </section>
 <section
-    class="bg-mybeige dark:bg-mydarkgreen grid rounded-3xl bg-[linear-gradient(to_right,#f683a0_1.5px,transparent_1.5px),linear-gradient(to_bottom,#f683a0_1.5px,transparent_1.5px)] md:bg-[size:30px_30px] bg-[size:20px_20px] border-b-[1.5px] border-mypink md:p-14 p-7  my-10 relative gap-5">
+    class="grid md:px-8 my-10 px-6 relative gap-5">
 
     <?php
     // 1) définir les arguments/filtres de la requête
@@ -77,13 +69,13 @@ get_header(); ?>
                 class="rounded-2xl grid-item group relative md:mb-10 md:mr-9 h-[243px] w-40 md:w-[300px] overflow-hidden bg-cover bg-center transition-all duration-300 ease-in-out md:h-[455px] block <?php echo esc_attr($slug); ?>">
                 <!-- Détails du projet -->
                 <div
-                    class="mr-5 absolute bottom-0 right-0 flex flex-col opacity-100 md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 text-left">
-                    <h2 class="soustitre text-mydarkgreen text-end "><?php the_title(); ?></h2>
+                    class="mr-5 absolute bottom-0 right-0 flex flex-col opacity-100 md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 text-left text-mybeige">
+                    <h2 class="texte text-end "><?php the_title(); ?></h2>
                     <div class="flex justify-end">
-                        <p class="texte font-bold lowercase text-mydarkgreen text-end ">
+                        <p class="sous-titre font-bold lowercase text-end ">
                             <?php echo wp_kses_post(get_field('pr_category')); ?>
                         </p>
-                        <p class="texte font-bold lowercase text-mydarkgreen text-end ">
+                        <p class="soustitre font-bold lowercase text-end ">
                             <?php $acf_date = get_field('date'); // Replace with your field name
                                 echo get_time_ago_acf($acf_date);
                             ?>
